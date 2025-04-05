c0285b6a69521d38e4038f0256276c774b133f4c
natewind <nrwind@proton.me>
2025-04-05 06:29:09 +0400

    Blog engine

--- blog/render.js
+++ blog/render.js
@@ -1,25 +1,31 @@
-// import markdownIt from "https://cdn.jsdelivr.net/npm/markdown-it@14.1.0/+esm"
+import markdownIt from "https://cdn.jsdelivr.net/npm/markdown-it@14.1.0/+esm"
 
-alert("Hello!");
+// TODO: Consider:
+// git submodule add https://github.com/markdown-it/markdown-it.git vendor/markdown-it
+// git submodule update --init --recursive
 
 // TODO: Automatically fix relative links?
 // TODO: Set <html lang="ru"> for the libertarian playlist?
-// document.addEventListener("DOMContentLoaded", function () {
-// 	function loadMarkdown() {
-// 		const hash = window.location.hash.substring(2);
-// 		const article = hash;
-// 		const filePath = `articles/${article}.md`;
 
-// 		fetch(filePath)
-// 			.then(response => response.text())
-// 			.then(text => {
-// 				const md = markdownIt();
-// 				const result = md.render(text);
-// 				document.body.innerHTML = result;
-// 			})
-// 			.catch(error => console.error("Error loading the markdown file:", error));
-// 	}
+document.addEventListener("DOMContentLoaded", function()
+{
+	function load_article()
+	{
+		const article = window.location.hash.substring(2);
+		const path = `articles/${article}.md`;
 
-// 	window.addEventListener("hashchange", loadMarkdown);
-// 	loadMarkdown();
-// });
+		fetch(path)
+			.then(response => response.text())
+			.then(text =>
+			{
+				const md = markdownIt();
+				const rendered = md.render(text);
+				document.body.innerHTML = rendered;
+				document.title = document.querySelector("h1").textContent;
+			})
+			.catch(error => console.error("Error loading the Markdown article: ", error));
+	}
+
+	window.addEventListener("hashchange", load_article);
+	load_article();
+});
