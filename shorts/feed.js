9ebc139fce1ec9b1a10c2b8434460879abb2cbfb
natewind <nrwind@proton.me>
2025-11-23 08:22:18 +0400

    feat: YouTube shorts feed viewer (WIP)

---
+++ shorts/feed.js
@@ -0,0 +1,5 @@
+import { XMLParser } from "https://cdn.jsdelivr.net/npm/fast-xml-parser@4.4.1/+esm";
+
+// Examples
+// Al Jokes: https://www.youtube.com/feeds/videos.xml?channel_id=UCupQd0e1leK4-Mj1wSfnkoQ
+// Alan Becker: https://www.youtube.com/feeds/videos.xml?channel_id=UCbKWv2x9t6u8yZoB3KcPtnw
