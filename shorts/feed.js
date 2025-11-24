93edbba346c4bc6243497df8019fe55f98137377
natewind <nrwind@proton.me>
2025-11-24 08:49:39 +0400

    fix(shorts): add CORS proxy

--- shorts/feed.js
+++ shorts/feed.js
@@ -5,19 +5,22 @@ import { XMLParser } from "https://cdn.jsdelivr.net/npm/fast-xml-parser@4.4.1/+e
 // Alan Becker: https://www.youtube.com/feeds/videos.xml?channel_id=UCbKWv2x9t6u8yZoB3KcPtnw
 
 
-const url_params = new URLSearchParams(window.location.search);
+// const url_params = new URLSearchParams(window.location.search);
 
-const channels = url_params
-	.get("channels")
-	.split(",")
-	.map(item => item.trim());
+// const channels = url_params
+// 	.get("channels")
+// 	.split(",")
+// 	.map(item => item.trim());
+
+const channels=["UCupQd0e1leK4-Mj1wSfnkoQ", "UCbKWv2x9t6u8yZoB3KcPtnw"]
 
 const parser = new XMLParser();
 
 async function fetch_rss(channel_id)
 {
 	const url = `https://www.youtube.com/feeds/videos.xml?channel_id=${channel_id}`
-	const response = await fetch(url);
+	const proxy = `https://api.allorigins.win/get?url=${encodeURIComponent(url)}`;
+	const response = await fetch(proxy);
 	const xml = await response.text();
 	const object = parser.parse(xml);
 	alert(object); // TEST
