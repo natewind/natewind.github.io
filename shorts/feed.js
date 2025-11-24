a5920f3f0402860fcb92e8cb38f63940b21458ca
natewind <nrwind@proton.me>
2025-11-24 09:13:18 +0400

    chore(shorts): test parsing RSS

--- shorts/feed.js
+++ shorts/feed.js
@@ -1,4 +1,4 @@
-import { XMLParser } from "https://cdn.jsdelivr.net/npm/fast-xml-parser@4.4.1/+esm";
+import { XMLParser } from "https://cdn.jsdelivr.net/npm/fast-xml-parser/+esm";
 
 // Examples
 // Al Jokes: https://www.youtube.com/feeds/videos.xml?channel_id=UCupQd0e1leK4-Mj1wSfnkoQ
@@ -11,17 +11,59 @@ const channels = url_params
 	.split(",")
 	.map(item => item.trim());
 
-const parser = new XMLParser();
+console.log(`Parsed channel IDs: ${channels}`) // TEST
+
+const parser = new XMLParser({ ignoreAttributes: false });
 
 async function fetch_rss(channel_id)
 {
 	const url = `https://www.youtube.com/feeds/videos.xml?channel_id=${channel_id}`
 	const proxy = `https://api.allorigins.win/get?url=${encodeURIComponent(url)}`;
 	const response = await fetch(proxy);
-	const xml = await response.text();
+	const text = await response.text();
+	const xml = JSON.parse(text).contents;
 	const object = parser.parse(xml);
-	alert(object.feed.title); // TEST
 	return object;
 }
 
-channels.forEach(it => fetch_rss(it));
+// TODO: Filter out non-shorts (by link regex)
+// TODO: Sort combined list
+// TODO: Refactor into iterator chain
+
+// <feed>
+// 	<title>al jokes</title>
+// 	<entry>
+// 		<title>certified rock inspector #shorts #comedy #funny</title>
+// 		<link rel="alternate" href="https://www.youtube.com/shorts/nDF5I8Sohag" />
+// 		<published>2025-11-22T21:25:47+00:00</published>
+// 		<media:group>
+// 			<media:thumbnail url="https://i3.ytimg.com/vi/nDF5I8Sohag/hqdefault.jpg" />
+// 		</media:group>
+// 	</entry>
+// </feed>
+
+for (const channel_id of channels)
+{
+	const rss = await fetch_rss(channel_id);
+	console.log(`Parsed channel RSS: ${rss}`) // TEST
+
+	const feed = rss.feed;
+	console.log(`Parsed channel feed: ${feed}`) // TEST
+
+	const title = feed.title;
+	console.log(`Title: ${title}`) // TEST
+
+	for (const entry of feed.entry)
+	{
+		// const video_id = entry["yt:videoId"]
+
+		console.log(`Video title: ${entry.title}`) // TEST
+		console.log(`Link: ${entry.link["@_href"]}`) // TEST
+		console.log(`Date: ${entry.published}`) // TEST
+		console.log(`Thumbnail: ${entry["media:group"]["media:thumbnail"]["@_url"]}`) // TEST
+
+		break; // TEST
+	}
+
+	break; // TEST
+}
